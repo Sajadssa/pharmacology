d1663d6e99ec81936bb31cac3d96cba2c2445bb4
Sajadssa <sajadsaeediazad0007@gmail.com>
2022-12-18 02:15:33 +0330

    new changes

--- index.js
+++ index.js
@@ -11,17 +11,15 @@ const cartItems = document.querySelector(".cart-items");
 const cartTotal = document.querySelector(".cart-total");
 const cartContent = document.querySelector(".cart-content");
 
-const cartStatus=document.querySelector('.cart-status');
+const cartStatus = document.querySelector(".cart-status");
 
-const clearCart = document.querySelector('.clear-cart');
+const clearCart = document.querySelector(".clear-cart");
 const toggler = document.querySelector(".toggler");
 const navbar = document.querySelector(".nav");
-
 
 let cart = [];
 
 let buttonsDOM = [];
-
 
 //get all products from product.js with class object
 
@@ -60,20 +58,19 @@ class UI {
       const id = btn.dataset.id;
       //check if product id is in cart or not?
 
-      const isInCart = cart.find((p) => p.id ===parseInt(id));
+      const isInCart = cart.find((p) => p.id === parseInt(id));
       if (isInCart) {
         btn.innerText = "موجود در سبد خرید ";
         btn.disabled = true;
-      
       }
       //if product id is not in cart
       btn.addEventListener("click", (e) => {
         // console.log(e.target.dataset.id);
         e.target.innerText = "موجود درسبد خرید";
         e.target.disabled = true;
-        cartItems.style.opacity="1";
-        if(!cart.length){
-          cartStatus.style.opacity="0";
+        cartItems.style.opacity = "1";
+        if (!cart.length) {
+          cartStatus.style.opacity = "0";
         }
         //1.get product from products in local storage
 
@@ -89,27 +86,23 @@ class UI {
 
         //5.display cart items
         this.addCartItem(addedProduct);
-
       });
-    
-      
     });
   }
   //calculation cartItems and totalPrice
   setCartValue(cart) {
     let tempCartItems = 0;
- const totalPrice=cart.reduce((acc, curr) => {
+    const totalPrice = cart.reduce((acc, curr) => {
       tempCartItems += curr.quantity; //2+1=3
       // console.log(tempCartItems);
       return curr.quantity * curr.price + acc;
     }, 0);
-   
+
     cartTotal.innerText = `قیمت کل:${parseFloat(totalPrice).toFixed(2)} تومان`;
     cartItems.innerText = tempCartItems;
-  
   }
   //Display cart items in show modal
- addCartItem(cart) {
+  addCartItem(cart) {
     const div = document.createElement("div");
     div.classList.add("cart-item");
     div.innerHTML = `<div><img class="cart-item-img" src=${cart.imageUrl} /></div>
@@ -127,108 +120,102 @@ class UI {
     cartContent.appendChild(div);
   }
 
-//6.remain cart after refresh browser
-setupApp() {
-  cart = Storage.getCart();
- 
-  this.setCartValue(cart);
-  
-  this.populateCart(cart);
-  
-}
+  //6.remain cart after refresh browser
+  setupApp() {
+    cart = Storage.getCart();
+
+    this.setCartValue(cart);
+
+    this.populateCart(cart);
+  }
 
   populateCart(cart) {
     cart.forEach((item) => this.addCartItem(item));
   }
 
-  cartLogic(){
-
+  cartLogic() {
     //clear cart button
-    clearCart.addEventListener('click',(e)=>{
+    clearCart.addEventListener("click", (e) => {
       // console.log('clear',e.target.value)
       this.clearCart();
       closeModalFunction();
-   
-    })
-    cartContent.addEventListener('click', (event) => {
-// console.log(event.target);
-if(event.target.classList.contains('fa-chevron-up')){
-  console.log(event.target.dataset.id);
-  const addQuantity=event.target;
-  //1.get item from cart
-  const addedItem=cart.find((cItem)=>cItem.id==addQuantity.dataset.id);
-  addedItem.quantity++;
-  //2.update cart value
-  this.setCartValue(cart);
-  //3.save cart
-  Storage.saveCart(cart);
-  // update cart item an ui
-  console.log(addQuantity.nextElementSibling);
-  addQuantity.nextElementSibling.innerText=addedItem.quantity;
-  
-}
-else if (event.target.classList.contains('fa-trash-alt')){
- const removeItem=event.target;
-//  console.log(event.target);
-  const id = removeItem.dataset.id;
-  console.log(id);
- const _removedItem=cart.find((c)=>c.id===parseInt(removeItem.dataset.id));
-  this.removeItem(_removedItem.id);
- this.setCartValue(cart);
- //update cart in storage
- Storage.saveCart(cart);
- //for delete item from cartContent:
- cartContent.removeChild(removeItem.parentElement);
- 
-}
-else if (event.target.classList.contains('fa-chevron-down')) {
-  // console.log(event.target.dataset.id);
-  const substractQuantity = event.target;
-  //1.get item from cart
-  const substractedItem = cart.find((cItem) => cItem.id == substractQuantity.dataset.id);
-  
-  if(substractedItem.quantity===1){
-    
-    this.removeItem(substractedItem.id);
-    cartContent.removeChild(substractQuantity.parentElement.parentElement);
-     
-    
-  }
-  if (!cart.length) cartStatus.style.opacity = "1";
-  cartItems.style.opacity = "0";
-  // console.log(substractQuantity.parentElement);
-  substractedItem.quantity--;
-  //2.update cart value
-  this.setCartValue(cart);
-  //3.save cart
-  Storage.saveCart(cart);
-  // update cart item an ui
-  // console.log(substractQuantity.previousElementSibling);
-  substractQuantity.previousElementSibling.innerText = substractedItem.quantity;
+    });
+    cartContent.addEventListener("click", (event) => {
+      // console.log(event.target);
+      if (event.target.classList.contains("fa-chevron-up")) {
+        console.log(event.target.dataset.id);
+        const addQuantity = event.target;
+        //1.get item from cart
+        const addedItem = cart.find(
+          (cItem) => cItem.id == addQuantity.dataset.id
+        );
+        addedItem.quantity++;
+        //2.update cart value
+        this.setCartValue(cart);
+        //3.save cart
+        Storage.saveCart(cart);
+        // update cart item an ui
+        console.log(addQuantity.nextElementSibling);
+        addQuantity.nextElementSibling.innerText = addedItem.quantity;
+      } else if (event.target.classList.contains("fa-trash-alt")) {
+        const removeItem = event.target;
+        //  console.log(event.target);
+        const id = removeItem.dataset.id;
+        console.log(id);
+        const _removedItem = cart.find(
+          (c) => c.id === parseInt(removeItem.dataset.id)
+        );
+        this.removeItem(_removedItem.id);
+        this.setCartValue(cart);
+        //update cart in storage
+        Storage.saveCart(cart);
+        //for delete item from cartContent:
+        cartContent.removeChild(removeItem.parentElement);
+      } else if (event.target.classList.contains("fa-chevron-down")) {
+        // console.log(event.target.dataset.id);
+        const substractQuantity = event.target;
+        //1.get item from cart
+        const substractedItem = cart.find(
+          (cItem) => cItem.id == substractQuantity.dataset.id
+        );
 
-}
-    })
-
-
+        if (substractedItem.quantity === 1) {
+          this.removeItem(substractedItem.id);
+          cartContent.removeChild(
+            substractQuantity.parentElement.parentElement
+          );
+        }
+        if (!cart.length) cartStatus.style.opacity = "1";
+        cartItems.style.opacity = "0";
+        // console.log(substractQuantity.parentElement);
+        substractedItem.quantity--;
+        //2.update cart value
+        this.setCartValue(cart);
+        //3.save cart
+        Storage.saveCart(cart);
+        // update cart item an ui
+        // console.log(substractQuantity.previousElementSibling);
+        substractQuantity.previousElementSibling.innerText =
+          substractedItem.quantity;
+      }
+    });
   }
 
-  clearCart(){
+  clearCart() {
     //loop on all the items and trigger remove for each one
-    cart.forEach((item)=>this.removeItem(item.id));
+    cart.forEach((item) => this.removeItem(item.id));
     // console.log(cartContent.children[0]);
-    while(cartContent.children.length){
+    while (cartContent.children.length) {
       cartContent.removeChild(cartContent.children[0]);
-      if(!cart.length) cartStatus.style.opacity = "1"; 
-       cartItems.style.opacity = "0";
-    
-
+      if (!cart.length) cartStatus.style.opacity = "1";
+      cartItems.style.opacity = "0";
     }
   }
 
-  removeItem(id){
+  removeItem(id) {
     //reusable method for single remove and clear all items in cart
     // console.log(id);
-    cart=cart.filter((cItems)=>cItems.id!==id);
+    cart = cart.filter((cItems) => cItems.id !== id);
     // console.log(cartContent.children);
     this.setCartValue(cart);
     Storage.saveCart(cart);
@@ -236,16 +223,12 @@ else if (event.target.classList.contains('fa-chevron-down')) {
     button.disabled = false;
     button.innerHTML = `
    اضافه کردن به سبد خرید`;
-   console.log(buttonsDOM);
-    
+    console.log(buttonsDOM);
   }
   getSingleButton(id) {
     // should be parseInt to get correct result
     return buttonsDOM.find((btn) => parseInt(btn.dataset.id) === parseInt(id));
   }
- 
-  
-
 }
 
 class Storage {
@@ -267,27 +250,26 @@ class Storage {
       ? JSON.parse(localStorage.getItem("cart"))
       : [];
   }
-
-  
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   const ui = new UI(0);
   // set up already added cart items
- ui.setupApp();
+  ui.setupApp();
   const products = new Products();
   const productsData = products.getProducts();
   // console.log(displayProducts);
-  (!cart.length) ? cartStatus.style.opacity = "1":cartItems.style.opacity = "1";
-  
+  !cart.length
+    ? (cartStatus.style.opacity = "1")
+    : (cartItems.style.opacity = "1");
+
   ui.displayProducts(productsData);
   //access to buttons
   ui.getCartBtns();
-  
+
   Storage.saveProducts(productsData);
   //  console.log(addToCartBtn);
   ui.cartLogic();
-   
 });
 
 //modal
@@ -309,12 +291,7 @@ backDrop.addEventListener("click", closeModalFunction);
 
 // navBar toggler
 toggler.addEventListener("click", () => {
- 
-  // console.log("clicked");
   navbar.classList.toggle("nav_expanded");
-  // cartBtn.style.opacity="0";
-  // logo.style.opacity="0";
-
 });
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll("section");
@@ -336,6 +313,3 @@ window.onscroll = () => {
     }
   });
 };
-
-
-// google map------------------
